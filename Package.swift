b765b3da24898e342c65418fc2d5298ac0373e69
Casey Law <tsukisa@proton.me>
2024-01-04 16:56:07 +0800

    fixed xcframework download path

--- Package.swift
+++ Package.swift
@@ -21,7 +21,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "VLCMediaLibraryKit",
-            url: "https://github.com/TeapotSoft/VLCMediaLibraryKit/releases/download/0.13.0a5/VLCMediaLibraryKit.xcframework.zip",
+            url: "https://github.com/TeapotSoft/VLCMediaLibraryKit/releases/download/0.13.0/VLCMediaLibraryKit.xcframework.zip",
             checksum: "6b78a7892b133aef1289ec7eee06921c03696b83c0cdb148399d6cca5aaf2c10"
         )
     ]
